94d32987341b844b744c8b2fde8e6b66fd2c11e8
jwaterfall <jack.a.waterfall@gmail.com>
2022-04-11 02:50:01 +0100

    Enabled PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
